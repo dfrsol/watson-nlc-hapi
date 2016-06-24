047bf3434e9790f5610a7ea536103cacc9894989
Josh Buselt <dfrsol@gmail.com>
2016-06-23 22:46:07 -0500

    Added Hapi unit testing framework with first basic test and style guide linting

---
+++ test/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// Load modules
+
+const Hapi = require('hapi');
+const Code = require('code');
+const Lab = require('lab');
+const Server = require('../lib');
+
+
+// Declare internals
+
+const internals = {};
+
+
+// Test shortcuts
+
+const lab = exports.lab = Lab.script();
+const describe = lab.experiment;
+const expect = Code.expect;
+const it = lab.test;
+
+
+describe('/index', () => {
+
+    it('starts server and returns hapi server object', (done) => {
+
+        Server.init(8000, (err, server) => {
+
+            expect(err).to.not.exist();
+            expect(server).to.be.instanceof(Hapi.Server);
+
+            server.stop(done);
+        });
+    });
+});
